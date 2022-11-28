ea3af11aebf38559116049a4ff314195ab23125f
Musab Abuasi <abuasimusab@gmail.com>
2022-11-28 12:37:17 +0200

    Adding configds and jdbi

---
+++ src/main/java/net/twitter/infra/Configuration.java
@@ -0,0 +1,46 @@
+package net.twitter.infra;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@Data
+@NoArgsConstructor
+public class Configuration {
+
+    private static Configuration instance = null;
+    private String auth0Domain;
+    private String auth0ClientId;
+    private String auth0ClientSecret;
+    private String dbUrl;
+    private String dbUser;
+    private String dbPassword;
+    private int dbMinPoolSize = 5;
+    private int dbMaxPoolSize = 25;
+    private long dbConnectionIdleTimeout = 30000L;
+
+    public static Configuration instance() {
+        if (instance == null) {
+            File file = new File("appconfig.yml");
+            if (file.exists()) {
+                ObjectMapper om = new ObjectMapper(new YAMLFactory());
+                om.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+                try {
+                    instance = om.readValue(file, Configuration.class);
+                } catch (IOException ex) {
+                    Logger.getLogger(Configuration.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            } else {
+                instance = new Configuration();
+            }
+        }
+        return instance;
+    }
+}
---
+++ src/main/java/net/twitter/provider/JdbiProvider.java
@@ -0,0 +1,51 @@
+package net.twitter.provider;
+
+import com.zaxxer.hikari.HikariDataSource;
+import net.twitter.infra.Configuration;
+import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.sqlobject.SqlObjectPlugin;
+import org.slf4j.LoggerFactory;
+
+public class JdbiProvider {
+    private Jdbi jdbi = null;
+    private static JdbiProvider instance = instance();
+    private HikariDataSource dataSource;
+    private JdbiProvider() {
+    }
+
+    public static JdbiProvider instance() {
+        if (instance == null) {
+            instance = new JdbiProvider();
+        }
+        return instance;
+    }
+
+    public void init() {
+        if (jdbi != null) {
+            jdbi = null;
+        }
+        if (dataSource != null) {
+            try {
+                dataSource.close();
+            } catch (Exception ex) {
+                LoggerFactory.getLogger(this.getClass()).error("Hikari data source is null");
+                } finally {
+                dataSource = null;
+            }
+        }
+        dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(Configuration.instance().getDbUrl());
+        dataSource.setUsername(Configuration.instance().getDbUser());
+        dataSource.setPassword(Configuration.instance().getDbPassword());
+        dataSource.setMinimumIdle(Configuration.instance().getDbMinPoolSize());
+        dataSource.setMaximumPoolSize(Configuration.instance().getDbMaxPoolSize());
+        dataSource.setIdleTimeout(Configuration.instance().getDbConnectionIdleTimeout());
+
+        jdbi = Jdbi.create(dataSource);
+        jdbi.installPlugin(new SqlObjectPlugin());
+    }
+
+    public Jdbi jdbi() {
+        return jdbi;
+    }
+}
