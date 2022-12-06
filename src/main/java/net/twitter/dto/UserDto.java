439d4aae3190c67fe463f829a53851a647e9814e
Musab Abuasi <abuasimusab@gmail.com>
2022-12-06 15:49:16 +0200

    [twitter-2] adding userDto

--- src/main/java/net/twitter/dto/UserDto.java
+++ src/main/java/net/twitter/dto/UserDto.java
@@ -1,10 +1,12 @@
 package net.twitter.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class UserDto {
     private String id;
     private String email;
