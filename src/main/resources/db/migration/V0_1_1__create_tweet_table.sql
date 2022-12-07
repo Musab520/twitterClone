c4964db499c16883ebcfb34d81fa022bd5c9f291
Musab Abuasi <abuasimusab@gmail.com>
2022-12-07 09:23:45 +0200

    [twitter-2] changing to authorId

--- src/main/resources/db/migration/V0_1_1__create_tweet_table.sql
+++ src/main/resources/db/migration/V0_1_1__create_tweet_table.sql
@@ -1,8 +1,8 @@
 CREATE TABLE `tweet` (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id VARCHAR(36),
+    author_id VARCHAR(36),
     title VARCHAR(60),
     message VARCHAR(280),
-    createdOn Date,
-    FOREIGN KEY (user_id) REFERENCES user(id)
+    created_on Date,
+    FOREIGN KEY (author_id) REFERENCES user(id)
 );
