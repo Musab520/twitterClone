504106ea6fbd019f815cfa1c51cfd693da926b6f
Musab Abuasi <abuasimusab@gmail.com>
2022-12-13 21:26:37 +0200

    [twitter-7] fixing TwitterDao

--- src/main/java/net/twitter/dao/TweetDao.java
+++ src/main/java/net/twitter/dao/TweetDao.java
@@ -1,9 +1,12 @@
 package net.twitter.dao;
 
 import net.twitter.dto.TweetDto;
+import net.twitter.dto.UserDto;
+import org.jdbi.v3.sqlobject.config.RegisterBeanMapper;
 import org.jdbi.v3.sqlobject.customizer.Bind;
 import org.jdbi.v3.sqlobject.customizer.BindBean;
 import org.jdbi.v3.sqlobject.locator.UseClasspathSqlLocator;
+import org.jdbi.v3.sqlobject.statement.SqlQuery;
 import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 
 public interface TweetDao {
@@ -11,8 +14,9 @@ public interface TweetDao {
     @UseClasspathSqlLocator
     void insert(@BindBean TweetDto tweetDto);
 
-    @SqlUpdate
+    @SqlQuery
     @UseClasspathSqlLocator
+    @RegisterBeanMapper(TweetDto.class)
     TweetDto find(@Bind("id") int id);
 
 }
