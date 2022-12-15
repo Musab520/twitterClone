d541d505b5ce050c86b631c795b766dc8469276a
Musab Abuasi <abuasimusab@gmail.com>
2022-12-15 18:23:33 +0200

    [twitter-7] fixed user id in session sttribute

--- src/main/java/net/twitter/controller/TweetController.java
+++ src/main/java/net/twitter/controller/TweetController.java
@@ -1,19 +1,27 @@
 package net.twitter.controller;
 
 import io.javalin.http.Context;
+import net.twitter.dto.TweetCreatedDto;
 import net.twitter.dto.TweetDto;
 import net.twitter.dto.UserDto;
 import net.twitter.service.TweetService;
 import net.twitter.service.UserService;
+import org.apache.commons.lang3.time.DateUtils;
+
+import java.util.Date;
 
 public class TweetController {
     public static void addTweet(Context ctx) {
-        TweetDto tweetDto = ctx.bodyAsClass(TweetDto.class);
+        TweetCreatedDto tweetCreatedDto = ctx.bodyAsClass(TweetCreatedDto.class);
+        TweetDto tweetDto = new TweetDto();
+        tweetDto.setAuthorId(ctx.sessionAttribute("user_id"));
+        tweetDto.setCreatedOn(new Date());
+        tweetDto.setMessage(tweetCreatedDto.getMessage());
         TweetService.getInstance().addTweet(tweetDto);
     }
 
     public static void findTweet(Context ctx) {
-        TweetDto tweetDto = TweetService.getInstance().findTweet(Integer.parseInt(ctx.pathParam("tweetId")));
+        TweetDto tweetDto = TweetService.getInstance().findTweet(ctx.pathParamAsClass("tweetId", Integer.class).getOrDefault(null));
         if (tweetDto == null) {
             ctx.status(404);
         } else {
