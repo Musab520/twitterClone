ac68e37953da94e89fa0ff223fbd9508bc4a2aa1
Musab Abuasi <abuasimusab@gmail.com>
2022-12-18 21:27:40 +0200

    [twitter-9] adding initial page

--- src/main/java/net/twitter/controller/TweetController.java
+++ src/main/java/net/twitter/controller/TweetController.java
@@ -1,6 +1,7 @@
 package net.twitter.controller;
 
 import io.javalin.http.Context;
+import net.twitter.dto.IdDto;
 import net.twitter.dto.TweetCreatedDto;
 import net.twitter.dto.TweetDto;
 import net.twitter.dto.UserDto;
@@ -17,7 +18,9 @@ public class TweetController {
         tweetDto.setAuthorId(ctx.sessionAttribute("user_id"));
         tweetDto.setCreatedOn(new Date());
         tweetDto.setMessage(tweetCreatedDto.getMessage());
-        ctx.json(TweetService.getInstance().addTweet(tweetDto));
+        IdDto idDto = new IdDto();
+        idDto.setId(TweetService.getInstance().addTweet(tweetDto));
+        ctx.json(idDto);
     }
 
     public static void findTweet(Context ctx) {
--- src/main/resources/vue/components/app-layout.vue
+++ src/main/resources/vue/components/app-layout.vue
@@ -28,7 +28,6 @@
                     class="white--text text-decoration-none d-flex align-center">
                     <v-icon color="white" class="me-3">{{ route.icon }}</v-icon>
                     {{ $trans(route.title) }}
-
                   </a>
                 </v-list-item>
 
@@ -50,40 +49,20 @@
               {{ $trans('homepage.navigation.homepage') }}
             </v-tab>
             <v-tab exact exact-path href="/equipments">
-              {{ $trans('homepage.navigation.equipments') }}
+              {{ $trans('homepage.navigation.newsFeed') }}
             </v-tab>
             <v-tab exact exact-path href="/companies">
-              {{ $trans('homepage.navigation.companies') }}
+              {{ $trans('homepage.navigation.profile') }}
             </v-tab>
             <v-tab exact exact-path href="/request-for-quotations">
-              {{ $trans('homepage.navigation.rfqs') }}
+              {{ $trans('homepage.navigation.Search') }}
             </v-tab>
           </v-tabs>
 
           <div class="d-flex ms-auto" style="height:100%">
-
             <div class="me-4">
               <language-switcher-component />
             </div>
-            <a class="d-flex ms-auto align-center" :href="window?.globalConfig?.portalUrl??'://'"/>
-              target="_blank">
-              <span class="white--text pe-2">
-                {{ $trans('homepage.navigation.portal') }}
-              </span>
-              <svg width="24" height="24" viewBox="0 0 24 24" fill="white" xmlns="http://www.w3.org/2000/svg">
-                <g clip-path="url(#clip0_1207_4188)">
-                  <path
-                    d="M19.375 19H5.375V5H12.375V3H5.375C4.265 3 3.375 3.9 3.375 5V19C3.375 20.1 4.265 21 5.375 21H19.375C20.475 21 21.375 20.1 21.375 19V12H19.375V19ZM14.375 3V5H17.965L8.135 14.83L9.545 16.24L19.375 6.41V10H21.375V3H14.375Z"
-                    fill="white" />
-                </g>
-                <defs>
-                  <clipPath id="clip0_1207_4188">
-                    <rect width="24" height="24" fill="white" transform="translate(0.375)" />
-                  </clipPath>
-                </defs>
-              </svg>
-            </a>
-
           </div>
 
 
@@ -112,37 +91,6 @@
               </template>
             </v-menu>
           </v-toolbar-title>
-          <v-divider color="white"></v-divider>
-
-          <v-list-item class="hover-w-10">
-            <a :href=`/` style="width:100%;height:56px;"
-              class="white--text text-decoration-none d-flex align-center">
-              <v-icon color="white" class="me-3">mdi-home-outline</v-icon>
-            {{ $trans('homepage.navigation.homepage') }}
-            </a>
-          </v-list-item>
-          <v-list-item class="hover-w-10">
-            <a :href=`/equipments` style="width:100%;height:56px;"
-              class="white--text text-decoration-none d-flex align-center">
-              <v-icon color="white" class="me-3">mdi-tractor-variant</v-icon>
-              {{ $trans('homepage.navigation.equipments') }}
-            </a>
-          </v-list-item>
-          <v-list-item class="hover-w-10">
-            <a :href=`/companies` style="width:100%;height:56px;"
-              class="white--text text-decoration-none d-flex align-center">
-              <v-icon color="white" class="me-3">mdi-domain</v-icon>
-              {{ $trans('homepage.navigation.companies') }}
-            </a>
-          </v-list-item>
-          <v-list-item class="hover-w-10">
-            <a :href=`/request-for-quotations` style="width:100%;height:56px;"
-              class="white--text text-decoration-none d-flex align-center">
-              <v-icon color="white" class="me-3">mdi-cash-clock </v-icon>
-              {{ $trans('homepage.navigation.rfqs') }}
-            </a>
-          </v-list-item>
-          <v-divider color="white"></v-divider>
           <v-list-item v-for="(route) in routesList" :key="route.href" class="hover-w-10">
             <a :href=route.href style="width:100%;height:56px;"
               class="white--text text-decoration-none d-flex align-center">
@@ -191,57 +139,16 @@ Vue.component("app-layout", {
       group: null,
       navHeight: '56px',
       routesList: [
-        {
-          href: '/my-company/info',
-          title: 'homepage.navigation.userMenu.companyInfo',
-          icon: 'mdi-home-city-outline ', 
-        },
-        {
-          href: '/my-company/my-equipments',
-          title: 'homepage.navigation.userMenu.myEquipments',
-          icon: 'mdi-tractor-variant',
-
-        },
-        {
-          href: '/my-company/project-list',
-          title: 'homepage.navigation.userMenu.projectCollections',
-          icon: 'mdi-bookmark-box-multiple-outline',
-
-        },
-        {
-          href: '/my-company/history',
-          title: 'homepage.navigation.userMenu.history',
-          icon: 'mdi-history',
-
-        },
-        {
-          href: '/my-company/reservations',
-          title: 'homepage.navigation.userMenu.currentReservations',
-          icon: 'mdi-calendar-check-outline',
-
-        },
-        {
-          href: '/my-company/rentals',
-          title: 'homepage.navigation.userMenu.currentRentals',
-          icon: 'mdi-car-key ',
-
-        },
-        {
-          href: '/my-company/bidding',
-          title: 'homepage.navigation.userMenu.currentBids',
-          icon: 'mdi-cash-check ',
-
-        },
       ]
     };
   },
   created() {
-    this.tab = sessionStorage.getItem('equiptal-main-navigation-selected-tab');
+    this.tab = sessionStorage.getItem('twitter-main-navigation-selected-tab');
     this.user = new LoadableData(`/api/users/${this.$javalin.state.userDetails.user_id}`);
   },
   methods: {
     handleChange(index) {
-      sessionStorage.setItem('equiptal-main-navigation-selected-tab', index);
+      sessionStorage.setItem('twitter-main-navigation-selected-tab', index);
     }
   }
 });
